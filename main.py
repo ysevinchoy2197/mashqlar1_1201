23409a1bbf6e8c4bf8384f435d2a40e198adafc8
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-01-12 21:49:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#1-misol
+a = [10, 20, 30, 40, 50]
+
+yangi_a = a[:1]
+print(yangi_a)
+
+#2-misol
+lst = [10, 20, 30, 40, 50, 60, 70, 80]
+
+oxirgi = lst[-1:]
+print(oxirgi)
+
+#3-misol
+lst = [10, 20, 30, 40, 50, 60, 70, 80]
+qism = lst[2:7]
+print(qism)
+
+#4-misol
+lst = [10, 20, 30, 40, 50, 60, 70, 80]
+birinchi_uch = lst[:3]
+print(birinchi_uch)
+
+#5-misol
+lst = [10, 20, 30, 40, 50, 60, 70, 80]
+oxirgi_tort = lst[-4:]
+print(oxirgi_tort)
